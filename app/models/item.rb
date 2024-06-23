65f52e67804c50966f5c5b55a7a09e03fbb52205
Allisonna <167255619+Allisonna@users.noreply.github.com>
2024-06-23 17:26:54 +0900

    配送料負担のプルダウン装着

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :shippingcost
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
