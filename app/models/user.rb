a94d10a8824fb04ece5d2377eb840d9497c9efb4
Allisonna <167255619+Allisonna@users.noreply.github.com>
2024-06-23 14:53:45 +0900

    レビュー修正＃２

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   attr_accessor :password_confirmation
 
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください。' }
