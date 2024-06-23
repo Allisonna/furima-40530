ed968ac2d316d48959f37237d8ed122cda7a0f5f
Allisonna <167255619+Allisonna@users.noreply.github.com>
2024-06-23 17:52:05 +0900

    発送元の地域のプルダウン装着

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,10 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :shippingcost
+  belongs_to :shippingarea
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
-
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
