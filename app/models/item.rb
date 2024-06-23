1d01134de6f59a149c5584bc2d8e472154e5e9a9
Allisonna <167255619+Allisonna@users.noreply.github.com>
2024-06-23 17:56:30 +0900

    発送までの日数のプルダウン装着

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,12 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :shippingcost
   belongs_to :shippingarea
+  belongs_to :shippingday
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
 end
