6d2c17ed35802a18b0f5ea97809a3c00ccc211be
Allisonna <167255619+Allisonna@users.noreply.github.com>
2024-07-24 17:07:46 +0900

    PAY.JPを用いたクレジットカードでの決済機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,6 +3,8 @@ class PurchasesController < ApplicationController
   before_action :redirect_if_not_author, only: [:index, :create]
 
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    @purchase = Purchase.new
     @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
   end
@@ -13,8 +15,10 @@ class PurchasesController < ApplicationController
     if @purchase_address.valid?
       @purchase_address.save
       update_item_sold_out(@purchase_address.item_id)
+      pay_item
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,7 +27,7 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:postcode, :shipping_area_id, :city, :address1, :address2,
-                                             :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+                                             :tel).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def redirect_if_not_author
@@ -36,5 +40,14 @@ class PurchasesController < ApplicationController
   def update_item_sold_out(item_id)
     @item = Item.find(item_id)
     @item.update(sold_out: true)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postcode, :shipping_area_id, :city, :address1, :address2, :tel, :purchase_id, :user_id, :item_id
+  attr_accessor :postcode, :shipping_area_id, :city, :address1, :address2, :tel, :purchase_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
@@ -9,8 +9,9 @@ class PurchaseAddress
     validates :city
     validates :address1
     validates :tel, format: { with: /\A\d+\z/, message: 'はハイフン無しの数字のみ入力してください' }
+    validates :token
   end
-  validates :shipping_area_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id:, item_id:)
